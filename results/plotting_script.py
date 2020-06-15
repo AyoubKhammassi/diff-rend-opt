ed1f41f512a4b12fc5a95498918a133c754cec1a
Mohamed Zouari <zouarimohamed@outlook.com>
2020-06-15 16:59:57 +0100

    plotting results (results folder)

---
+++ results/plotting_script.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+#Parses the output file and returns a matrix with error values
+def parse(resultsFile):
+    
+    results = open(resultsFile)
+    resultLines = results.readlines()
+
+    iteration = []
+    error = []
+
+    for line in resultLines:
+        words = line.split(" ")
+        iteration.append(int(words[1]))
+        error.append(float(words[4].strip('\n')))
+        
+    results = [iteration, error]
+    return results
+
+
+classicResults = parse('ClassicResults.txt')
+maskResults    = parse('MaskResults.txt')
+
+plt.figure(figsize=(40,40))
+plt.plot(classicResults[0], classicResults[1] ,label='classic results')
+plt.plot(maskResults[0], maskResults[1] ,label='mask results')
+
+
+plt.ylabel('error')
+plt.xlabel('iterations')
+plt.show()
